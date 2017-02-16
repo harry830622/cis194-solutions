9568550727271298c8e56991332e4d0691904bd1
Harry Chang <harry830622@gmail.com>
2017-02-16 20:40:00 +0800

    Complete HW3-ex2

--- 03-rec-poly/Golf.hs
+++ 03-rec-poly/Golf.hs
@@ -9,3 +9,15 @@ skips elements = [every i elements | i <- [1 .. (length elements)]]
 every :: Int -> [a] -> [a]
 every n elements =
   [elements !! i | i <- [n - 1, n - 1 + n .. (length elements) - 1]]
+
+-- Exercise 2
+localMaxima :: [Integer] -> [Integer]
+localMaxima integers =
+  [integers !! i | i <- [1 .. (length integers) - 1],
+    not (isGreaterThanNext !! (i - 1)) && isGreaterThanNext !! i]
+  where isGreaterThanNext = makeIsGreaterThanNext integers
+
+makeIsGreaterThanNext :: [Integer] -> [Bool]
+makeIsGreaterThanNext integers =
+  [integers !! i > integers !! (i + 1) | i <- [0 .. (length integers) - 2]] ++
+  (False : [])
