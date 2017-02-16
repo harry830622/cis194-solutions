e594fe66d2dd503d56d1f807dccdc5fed8727590
Harry Chang <harry830622@gmail.com>
2017-02-16 22:39:37 +0800

    Complete HW3

--- 03-rec-poly/Golf.hs
+++ 03-rec-poly/Golf.hs
@@ -21,3 +21,26 @@ makeIsGreaterThanNext :: [Integer] -> [Bool]
 makeIsGreaterThanNext integers =
   [integers !! i > integers !! (i + 1) | i <- [0 .. (length integers) - 2]] ++
   (False : [])
+
+-- Exercise 3
+histogram :: [Integer] -> String
+histogram integers = (unlines . reverse) $ "0123456789" : "==========" : [] ++
+                                           map makeStars
+                                               (take maxCount lineCounts)
+  where counts = makeCounts integers
+        maxCount = fromInteger $ maximum counts
+        decrement = map ((+) (-1))
+        lineCounts = (iterate decrement counts)
+
+makeCounts :: [Integer] -> [Integer]
+makeCounts integers = foldl incrementCounts initialCounts integers
+  where initialCounts = replicate 10 0
+
+incrementCounts :: [Integer] -> Integer -> [Integer]
+incrementCounts integers x = take n integers ++
+                             (integers !! n + 1) : [] ++
+                             drop (n + 1) integers
+                where n = fromInteger x
+
+makeStars :: [Integer] -> String
+makeStars = map (\x -> if x > 0 then '*' else ' ')
