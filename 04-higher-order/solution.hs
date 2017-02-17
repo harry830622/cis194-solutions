799437b514dac9e77e4e2efa4a4f4db257ff3124
Harry Chang <harry830622@gmail.com>
2017-02-17 20:30:31 +0800

    Complete HW4-ex3

--- 04-higher-order/solution.hs
+++ 04-higher-order/solution.hs
@@ -10,3 +10,20 @@ fun2 =
   filter even .
   takeWhile (>1) .
   iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
+
+-- Exercise 2
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+-- foldTree :: [a] -> Tree a
+
+-- Exercise 3
+xor :: [Bool] -> Bool
+xor = odd . length . filter (\x -> x)
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base = foldr (flip f) base
