dd07e4a8d9d19dd6f71e047820d07455348c01a3
Harry Chang <harry830622@gmail.com>
2017-02-18 14:49:27 +0800

    Improve HW5

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -2,9 +2,9 @@
 
 module Calc where
 
-import Data.Maybe
 import ExprT
 import Parser
+import Data.Maybe
 
 -- Exercise 1
 eval :: ExprT -> Integer
@@ -25,31 +25,31 @@ class Expr a where
   mul :: a -> a -> a
 
 instance Expr ExprT where
-  lit x = Lit x
-  add x y = Add x y
-  mul x y = Mul x y
+  lit = Lit
+  add = Add
+  mul = Mul
 
 -- Exercise 4
 instance Expr Integer where
-  lit x = x
-  add x y = x + y
-  mul x y = x * y
+  lit = id
+  add = (+)
+  mul = (*)
 
 instance Expr Bool where
-  lit x = x > 0
-  add x y = x || y
-  mul x y = x && y
+  lit = (>0)
+  add = (||)
+  mul = (&&)
 
 newtype MinMax = MinMax Integer deriving (Eq, Show)
 newtype Mod7 = Mod7 Integer deriving (Eq, Show)
 
 instance Expr MinMax where
-  lit x = MinMax x
+  lit = MinMax
   add (MinMax x) (MinMax y) = MinMax $ max x y
   mul (MinMax x) (MinMax y) = MinMax $ min x y
 
 instance Expr Mod7 where
-  lit x = Mod7 x
+  lit x = Mod7 $ x `mod` 7
   add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
   mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
 
