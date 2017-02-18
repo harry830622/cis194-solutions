bf1b1a8176ca73f35ff1c0b5892bf8e056cab086
Harry Chang <harry830622@gmail.com>
2017-02-18 12:55:48 +0800

    Complete HW5-ex2

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -2,10 +2,18 @@
 
 module Calc where
 
+import Data.Maybe
 import ExprT
+import Parser
 
 -- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add x y) = eval x + eval y
 eval (Mul x y) = eval x * eval y
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr str | isNothing expr = Nothing
+            | otherwise = Just $ (eval . fromJust) expr
+  where expr = parseExp Lit Add Mul str
