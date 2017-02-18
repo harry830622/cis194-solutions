7d76cba69c836895cea1afdcbdd5dee6e1cd893b
Harry Chang <harry830622@gmail.com>
2017-02-18 12:44:41 +0800

    Complete HW5-ex1

---
+++ 05-type-classes/Calc.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Calc where
+
+import ExprT
+
+-- Exercise 1
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
