deb48544fbc70a2fecb807a8257206ddea7b9bbc
Harry Chang <harry830622@gmail.com>
2017-02-18 13:19:59 +0800

    Complete HW5-ex3

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -17,3 +17,14 @@ evalStr :: String -> Maybe Integer
 evalStr str | isNothing expr = Nothing
             | otherwise = Just $ (eval . fromJust) expr
   where expr = parseExp Lit Add Mul str
+
+-- Exercise 3
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit x = Lit x
+  add x y = Add x y
+  mul x y = Mul x y
