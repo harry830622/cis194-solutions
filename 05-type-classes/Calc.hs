f8d22b3045bbdb6de329bed6710a884703d34a6f
Harry Chang <harry830622@gmail.com>
2017-02-18 13:59:51 +0800

    Complete HW5-ex4

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -28,3 +28,30 @@ instance Expr ExprT where
   lit x = Lit x
   add x y = Add x y
   mul x y = Mul x y
+
+-- Exercise 4
+instance Expr Integer where
+  lit x = x
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit x = x > 0
+  add x y = x || y
+  mul x y = x && y
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+instance Expr Mod7 where
+  lit x = Mod7 x
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+testExp :: Expr a => String -> Maybe a
+testExp = parseExp lit add mul
