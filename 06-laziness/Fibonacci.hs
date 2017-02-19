b528bf56e2b4c4cdf02c85bc04cdcf8913ed276c
Harry Chang <harry830622@gmail.com>
2017-02-19 19:10:16 +0800

    Complete HW6-ex2

--- 06-laziness/Fibonacci.hs
+++ 06-laziness/Fibonacci.hs
@@ -10,3 +10,7 @@ fib n = fib (n - 2) + fib (n - 1)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0 ..]
+
+-- Exercise 2
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : [fibs2 !! (i - 2) + fibs2 !! (i - 1) | i <- [2 ..]]
