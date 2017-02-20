bca3c1b76562628a07f9150425e9a2c06634a04b
Harry Chang <harry830622@gmail.com>
2017-02-20 13:02:23 +0800

    Complete HW6-ex3&4

--- 06-laziness/Fibonacci.hs
+++ 06-laziness/Fibonacci.hs
@@ -14,3 +14,23 @@ fibs1 = map fib [0 ..]
 -- Exercise 2
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : [fibs2 !! (i - 2) + fibs2 !! (i - 1) | i <- [2 ..]]
+
+-- Exercise 3
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+-- Exercise 4
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f seed = Cons seed (streamFromSeed f next)
+  where next = f seed
