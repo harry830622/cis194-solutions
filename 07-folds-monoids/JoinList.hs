79f92dd04f1671d9bd23def05daeada25c9aab39
Harry Chang <harry830622@gmail.com>
2017-03-06 20:36:10 +0800

    Complete HW7-ex1

--- 07-folds-monoids/JoinList.hs
+++ 07-folds-monoids/JoinList.hs
@@ -1,3 +1,16 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module JoinList where
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) x y = Append (tag x) x y
+
+tag :: Monoid m => JoinList m a -> m
+tag (Single m _) = m
+tag (Append m _ _) = m
+tag _ = mempty
