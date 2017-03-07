fdb4698a7eac759ce4d2e2f7b59172703f4d39e1
Harry Chang <harry830622@gmail.com>
2017-03-07 13:43:41 +0800

    Complete HW7-ex2

--- 07-folds-monoids/JoinList.hs
+++ 07-folds-monoids/JoinList.hs
@@ -11,7 +11,7 @@ data JoinList m a = Empty
                   | Append m (JoinList m a) (JoinList m a)
   deriving (Eq, Show)
 
--- Excersise 1
+-- Exercise 1
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) x y = Append (tag x <> tag y) x y
 
@@ -20,7 +20,7 @@ tag (Single m _) = m
 tag (Append m _ _) = m
 tag _ = mempty
 
--- Excersise 2
+-- Exercise 2
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ i _ | i < 0 = Nothing
@@ -37,3 +37,13 @@ dropJ _ (Single _ _) = Empty
 dropJ i (Append _ l r) | i <= l_size = (dropJ i l) +++ r
                        | otherwise = dropJ (i - l_size) r
   where l_size = getSize . size $ tag l
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ i _ | i <= 0 = Empty
+takeJ _ j@(Single _ _) = j
+takeJ i (Append _ l r) | i <= l_size = takeJ i l
+                       | otherwise = l +++ takeJ (i - l_size) r
+  where l_size = getSize . size $ tag l
+
+-- Exercise 3
